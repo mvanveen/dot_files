d3a4ae83cc9bbc7d4d1bcde54a81c34242348ba6
Michael Van Veen <michael@mvanveen.net>
2014-08-04 05:02:22 -0700

    Update and rename synergy.conf to karabiner-import.sh

---
+++ karabiner-import.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set remap.consumer_to_fkeys_f5 1
+/bin/echo -n .
+$cli set remap.fkeys_to_consumer_f10 1
+/bin/echo -n .
+$cli set remap.fkeys_to_consumer_f1 1
+/bin/echo -n .
+$cli set remap.hhkmode 1
+/bin/echo -n .
+$cli set remap.pc_application2commandR_comamndLspace 1
+/bin/echo -n .
+$cli set option.extra_cursor_optionL_kjhl 1
+/bin/echo -n .
+$cli set remap.fkeys_to_consumer_f7 1
+/bin/echo -n .
+$cli set remap.shiftEscape2tilde 1
+/bin/echo -n .
+$cli set remap.hhkmode_vi_cursor 1
+/bin/echo -n .
+$cli set remap.enter2controlR_MacBookOnly 1
+/bin/echo -n .
+/bin/echo
